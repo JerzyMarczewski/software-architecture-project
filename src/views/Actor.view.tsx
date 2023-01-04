e49f88c188ecefe04357dbd1919e6e5e1b71e1aa
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2023-01-04 18:29:51 +0100

    updated Actor view and container

--- src/views/Actor.view.tsx
+++ src/views/Actor.view.tsx
@@ -1,7 +1,10 @@
 import { ReactElement } from "react";
+import { Actor } from "../helpers/types";
 
-const ActorView = (): ReactElement => {
-	return <div>ActorPage</div>;
+const ActorView: React.FC<{ actor: Actor | undefined }> = ({ actor }): ReactElement => {
+	if (actor === undefined) return <div>Loading...</div>;
+
+	return <div>Movie id: {actor.name}</div>;
 };
 
 export default ActorView;
