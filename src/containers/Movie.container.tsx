dcd1d15a215174b7cc43cef8a3119101af5718e2
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2023-01-04 17:53:53 +0100

    moved Movie interface to helpers

--- src/containers/Movie.container.tsx
+++ src/containers/Movie.container.tsx
@@ -3,38 +3,7 @@ import { useParams } from "react-router";
 import MovieView from "../views/Movie.view";
 import axios from "axios";
 
-interface Movie {
-	adult: boolean;
-	backdrop_path: string | null;
-	belongs_to_collection: null | object;
-	budget: number;
-	genres: Array<{ id: number; name: string }>;
-	homepage: string | null;
-	id: number;
-	imdb_id: string | null;
-	original_language: string;
-	original_title: string;
-	overview: string | null;
-	popularity: number;
-	poster_path: string | null;
-	production_companies: Array<{
-		name: string;
-		id: number;
-		logo_path: string | null;
-		origin_country: string;
-	}>;
-	production_countries: Array<{ iso_3166_1: string; name: string }>;
-	release_date: string;
-	revenue: number;
-	runtime: number | null;
-	spoken_languages: Array<{ iso_639_1: string; name: string }>;
-	status: string;
-	tagline: string | null;
-	title: string;
-	video: boolean;
-	vote_average: number;
-	vote_count: number;
-}
+import {Movie} from "../helpers/types"
 
 const MovieContainer = (): ReactElement => {
 	const { movieId } = useParams();
--- src/views/Movie.view.tsx
+++ src/views/Movie.view.tsx
@@ -1,37 +1,6 @@
 import React, { ReactElement } from "react";
 
-interface Movie {
-	adult: boolean;
-	backdrop_path: string | null;
-	belongs_to_collection: null | object;
-	budget: number;
-	genres: Array<{ id: number; name: string }>;
-	homepage: string | null;
-	id: number;
-	imdb_id: string | null;
-	original_language: string;
-	original_title: string;
-	overview: string | null;
-	popularity: number;
-	poster_path: string | null;
-	production_companies: Array<{
-		name: string;
-		id: number;
-		logo_path: string | null;
-		origin_country: string;
-	}>;
-	production_countries: Array<{ iso_3166_1: string; name: string }>;
-	release_date: string;
-	revenue: number;
-	runtime: number | null;
-	spoken_languages: Array<{ iso_639_1: string; name: string }>;
-	status: string;
-	tagline: string | null;
-	title: string;
-	video: boolean;
-	vote_average: number;
-	vote_count: number;
-}
+import {Movie} from "../helpers/types"
 
 const MovieView: React.FC<{ movie: Movie | undefined }> = ({ movie }): ReactElement => {
 	if (movie === undefined) return <div>Loading...</div>;
