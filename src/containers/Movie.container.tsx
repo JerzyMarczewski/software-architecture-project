6392f1b4f8d2622d927f0357c4768d88c29981f8
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2023-01-04 18:30:46 +0100

    updated Movie container and view

--- src/containers/Movie.container.tsx
+++ src/containers/Movie.container.tsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router";
 import MovieView from "../views/Movie.view";
 import axios from "axios";
 
-import {Movie} from "../helpers/types"
+import { Movie } from "../helpers/types";
 
 const MovieContainer = (): ReactElement => {
 	const { movieId } = useParams();
@@ -15,12 +15,12 @@ const MovieContainer = (): ReactElement => {
 
 		void (async () => {
 			try {
-				const data = await axios.get(
+				const response = await axios.get(
 					`https://api.themoviedb.org/3/movie/${movieId}?api_key=c84516f8b4384b587d79549a2ae95883&language=en-US`,
 				);
-				if (data.status !== 200) throw new Error();
+				if (response.status !== 200) throw new Error();
 
-				setMovie(data.data);
+				setMovie(response.data);
 			} catch (err) {
 				console.log(err);
 			}
--- src/views/Movie.view.tsx
+++ src/views/Movie.view.tsx
@@ -1,6 +1,5 @@
 import React, { ReactElement } from "react";
-
-import {Movie} from "../helpers/types"
+import { Movie } from "../helpers/types";
 
 const MovieView: React.FC<{ movie: Movie | undefined }> = ({ movie }): ReactElement => {
 	if (movie === undefined) return <div>Loading...</div>;
