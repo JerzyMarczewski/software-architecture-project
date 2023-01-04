fead23ce876e7dfded16db3c98dde80d3a512ba3
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2023-01-04 16:00:31 +0100

    changed app routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import { ReactElement } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
-import ActorPage from "./views/ActorPage";
-import Home from "./views/Home";
-import MoviePage from "./views/MoviePage";
+import ActorContainer from "./containers/Actor.container";
+import HomeContainer from "./containers/Home.container";
+import MovieContainer from "./containers/Movie.container";
 
 function App(): ReactElement {
 	return (
 		<div className="App">
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/movies/:movieId" element={<MoviePage />} />
-					<Route path="/actors/:actorId" element={<ActorPage />} />
+					<Route path="/" element={<HomeContainer />} />
+					<Route path="/movies/:movieId" element={<MovieContainer />} />
+					<Route path="/actors/:actorId" element={<ActorContainer />} />
+					<Route path="*" element={<Navigate to="/" />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
