95aed98971c04b8ccd291b8b976427b66b45cd51
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2023-01-06 18:46:55 +0100

    added images interface

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -123,6 +123,28 @@ export interface SimilarMovies {
 	total_results: number;
 }
 
+export interface Images {
+	id: number;
+	backdrops: Array<{
+		aspect_ratio: number;
+		file_path: string;
+		height: number;
+		iso_639_1: null | string;
+		vote_average: number;
+		vote_count: number;
+		width: number;
+	}>;
+	posters: Array<{
+		aspect_ratio: number;
+		file_path: string;
+		height: number;
+		iso_639_1: string | null;
+		vote_average: number;
+		vote_count: number;
+		width: number;
+	}>;
+}
+
 export type Section = "top_rated" | "popular" | "upcoming";
 
 export type Status = "loading" | "error" | "ok";
