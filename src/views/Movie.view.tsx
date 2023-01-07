6d413996453b6679f76a1fa23ec610a72ff9f9c4
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2023-01-07 17:59:30 +0100

    moved header jsx element up to the container

--- src/views/Movie.view.tsx
+++ src/views/Movie.view.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from "react";
-import { Carousel } from "react-responsive-carousel";
+// import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Movie, MovieCredits, SimilarMovies, Images } from "../helpers/types";
 import styles from "./Movie.module.css";
@@ -10,67 +10,15 @@ const MovieView: React.FC<{
 	credits: MovieCredits | undefined;
 	similar: SimilarMovies | undefined;
 	images: Images | undefined;
-}> = ({ movie, credits, similar, images }): ReactElement => {
+	header: JSX.Element;
+}> = ({ movie, credits, similar, images, header }): ReactElement => {
 	if (movie === undefined) return <div>Loading...</div>;
-
-	const image: JSX.Element | null =
-		movie.poster_path !== null ? (
-			<img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" />
-		) : null;
-
-	const director: string =
-		credits?.crew.find((person) => person.job === "Director")?.name ?? "unknown";
-
-	const writer: string =
-		credits?.crew
-			.filter((person) => person.department === "Writing" && person.job === "Story")
-			.map((person) => person.name)
-			.join(", ") ?? "unknown";
-
-	const genres: string = Object.keys(movie.genres)
-		.map((key: number | string) => movie.genres[key as number].name)
-		.join(", ");
-
-	const production: string = Object.keys(movie?.production_countries)
-		.map((key: number | string) => movie.production_countries[key as number].name)
-		.join(", ");
-
-	const release: string = movie?.release_date ?? "unknown";
-
-	const header = (
-		<div className={styles.header}>
-			{image}
-			<div className={styles.infoAndTitle}>
-				<h1>{movie.title}</h1>
-				<div className={styles.infoContainer}>
-					<div>Director</div>
-					<div>{director}</div>
-					<div>Writer</div>
-					<div>{writer}</div>
-					<div>Genre</div>
-					<div>{genres}</div>
-					<div>Production</div>
-					<div>{production}</div>
-					<div>Release</div>
-					<div>{release}</div>
-				</div>
-			</div>
-		</div>
-	);
 
 	const overview = (
 		<div className={styles.overviewContainer}>
 			<h3>Overview:</h3>
 			<div className={styles.overview}>{movie.overview}</div>
 		</div>
-	);
-
-	const imagesCarousel = (
-		<Carousel showArrows={true}>
-			<div>{image}</div>
-			<div>{image}</div>
-			<div>{image}</div>
-		</Carousel>
 	);
 
 	const cast = (
@@ -103,7 +51,6 @@ const MovieView: React.FC<{
 		<div className={styles.movieView}>
 			{header}
 			{overview}
-			{imagesCarousel}
 			{cast}
 			{suggestions}
 		</div>
