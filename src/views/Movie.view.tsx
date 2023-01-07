749abe1f4e1a7e807413959c5f8b62e64ba8685d
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2023-01-07 22:07:20 +0100

    removed unnecesarry props from movie view

--- src/views/Movie.view.tsx
+++ src/views/Movie.view.tsx
@@ -1,20 +1,15 @@
 import React, { ReactElement } from "react";
 // import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Movie, MovieCredits, SimilarMovies, Images } from "../helpers/types";
 import styles from "./Movie.module.css";
 
 const MovieView: React.FC<{
-	movie: Movie | undefined;
-	credits: MovieCredits | undefined;
-	similar: SimilarMovies | undefined;
-	images: Images | undefined;
 	header: JSX.Element;
 	overview: JSX.Element;
 	cast: JSX.Element;
 	suggestions: JSX.Element;
-}> = ({ movie, credits, similar, images, header, overview, cast, suggestions }): ReactElement => {
-	if (movie === undefined) return <div>Loading...</div>;
+}> = ({ header, overview, cast, suggestions }): ReactElement => {
+	// if (movie === undefined) return <div>Loading...</div>;
 
 	return (
 		<div className={styles.movieView}>
