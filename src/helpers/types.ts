7f431a6a111145f22022fc505ecb64d65e30154d
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2023-01-13 10:50:45 +0100

    added PersonMovieCredits interface

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -48,6 +48,47 @@ export interface Person {
 	homepage: null | string;
 }
 
+export interface PersonMovieCredits {
+	cast: Array<{
+		character: string;
+		credit_id: string;
+		release_date: string;
+		vote_count: number;
+		video: boolean;
+		adult: boolean;
+		vote_average: number;
+		title: string;
+		genre_ids: number[];
+		original_language: string;
+		original_title: string;
+		popularity: number;
+		id: number;
+		backdrop_path: string | null;
+		overview: string;
+		poster_path: string | null;
+	}>;
+	crew: Array<{
+		id: number;
+		department: string;
+		original_language: string;
+		original_title: string;
+		job: string;
+		overview: string;
+		vote_count: number;
+		video: boolean;
+		poster_path: string | null;
+		backdrop_path: string | null;
+		title: string;
+		popularity: number;
+		genre_ids: number[];
+		vote_average: number;
+		adult: boolean;
+		release_date: string;
+		credit_id: string;
+	}>;
+	id: number;
+}
+
 export interface CastMember {
 	adult: boolean;
 	gender: number | null;
@@ -75,6 +116,12 @@ export interface CrewMember {
 	credit_id: string;
 	department: string;
 	job: string;
+}
+
+export interface MovieCredits {
+	id: number;
+	cast: CastMember[];
+	crew: CrewMember[];
 }
 
 export interface SimilarMovies {
@@ -119,48 +166,6 @@ export interface Images {
 		vote_count: number;
 		width: number;
 	}>;
-}
-
-export interface MovieCredits {
-	cast: Array<{
-		character: string;
-		credit_id: string;
-		release_date: string;
-		vote_count: number;
-		video: boolean;
-		adult: boolean;
-		vote_average: number;
-		title: string;
-		genre_ids: number[];
-		original_language: string;
-		original_title: string;
-		popularity: number;
-		id: number;
-		backdrop_path: string | null;
-		overview: string;
-		poster_path: string | null;
-	}>;
-	crew: Array<{
-		id: number;
-		department: string;
-		original_language: string;
-		original_title: string;
-		job: string;
-		overview: string;
-		vote_count: number;
-		video: boolean;
-		poster_path: string | null;
-		backdrop_path: string | null;
-		title: string;
-		popularity: number;
-		genre_ids: number[];
-		vote_average: number;
-		adult: boolean;
-		release_date: string;
-		credit_id: string;
-	}>;
-
-	id: number;
 }
 
 export type Section = "top_rated" | "popular" | "upcoming" | "search";
