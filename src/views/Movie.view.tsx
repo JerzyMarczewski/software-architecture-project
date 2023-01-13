091d3e4159122744df3e4d6124768b04faecd1e7
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2023-01-13 10:22:16 +0100

    changed isLoaded type:
    :

--- src/views/Movie.view.tsx
+++ src/views/Movie.view.tsx
@@ -4,13 +4,13 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import styles from "./Movie.module.css";
 
 const MovieView: React.FC<{
-	isLoaded: Boolean;
+	isLoaded: boolean;
 	header: JSX.Element;
 	overview: JSX.Element;
 	cast: JSX.Element;
 	suggestions: JSX.Element;
 }> = ({ isLoaded, header, overview, cast, suggestions }): ReactElement => {
-	if (isLoaded === false) return <div>Loading...</div>;
+	if (!isLoaded) return <div>Loading...</div>;
 
 	return (
 		<div className={styles.movieView}>
