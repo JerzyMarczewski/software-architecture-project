f686b9b05abedc7d2c1d4d7f430897d273b47446
wojtek <wojtekpietraszuk@gmail.com>
2023-01-17 10:57:04 +0100

    Clean a component
    Remove commented code and add window scroll to 0, 0

--- src/containers/FavoriteMovies.container.tsx
+++ src/containers/FavoriteMovies.container.tsx
@@ -5,36 +5,7 @@ import { FavoriteMoviesContext } from "../context/favorite-movies-context";
 const FavoriteMoviesContainer = (): ReactElement => {
 	const { movies } = useContext(FavoriteMoviesContext);
 
-	// const getFavoriteMovies = async (): Promise<void> => {
-	// 	setStatus("loading");
-
-	// 	try {
-	// 		const userId = (await supabase.auth.getUser())?.data.user?.id;
-
-	// 		const { data, error, statusText } = await supabase
-	// 			.from("movie")
-	// 			.select()
-	// 			.eq("userId", userId);
-
-	// 		if (error !== null) {
-	// 			setStatus("error");
-	// 			throw new Error(statusText);
-	// 		}
-
-	// 		setStatus("ok");
-	// 		setFavoriteMovies(data);
-	// 	} catch (error) {
-	// 		if (error instanceof Error) {
-	// 			alert(error.message);
-	// 		}
-	// 	}
-	// };
-
-	// useEffect(() => {
-	// 	void (async () => {
-	// 		await getFavoriteMovies();
-	// 	})();
-	// }, []);
+	window.scrollTo(0, 0);
 
 	return <FavoriteMoviesView movies={movies} />;
 };
